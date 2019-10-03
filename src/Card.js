1b80c9cbd4d397c26b046914fb81504b5c1d95ef
Rana Bhattacharyya <ranab33@gmail.com>
2019-10-03 10:44:50 -0400

    randomly transforming cards

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import './Card.css';
 
 class Card extends Component {
+    constructor(props) {
+        super(props);
+        let angle = Math.random() * 90 - 45;
+        let xpos = Math.random() * 40 - 20;
+        let ypos = Math.random() * 40 - 20;
+        this._transform = `translate(${xpos}px, ${ypos}px) rotate(${angle}deg)`;
+    }
     render() {
+
         return (
-            <img className='Card' src={this.props.image} alt={this.props.name} />
+            <img style={{ transform: this._transform }}
+                className='Card'
+                src={this.props.image}
+                alt={this.props.name} />
         );
     }
 }
